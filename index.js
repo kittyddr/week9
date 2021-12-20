07f838d55e08aa3eb0f1b960766064ebaaebd214
kittyddr <eloisanyan@gmail.com>
2021-12-20 16:52:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const {pool} = require('./pgConfig')
+const PORT = process.env.PORT || 8080
+const app = express()
+app.get('/', (req, res) =>
+{
+    const sql1 = 'select * from задания;'
+    pool.query(sql1, (err, resp) =>
+    {
+        if(err) console.log(err)
+        else
+        {
+            res.render('problemset.ejs', {rows: resp.rows})
+        }
+    })
+})
+
+app.listen(PORT, ()=> console.log('Слушаю порт:', PORT))
