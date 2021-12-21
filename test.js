d2e9f7ca63e198a326d896b961145d27b0ff3b5e
kittyddr <eloisanyan@gmail.com>
2021-12-21 10:24:30 +0000

    commit

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const {pool} = require('./pgConfig')
-pool.query('select * from задания;', (err, res) =>
-{
-    if(err) console.log('Ошибка' , err)
-    else
-    {
-        console.log(res.rows)
-        pool.end()
-    }
-})
+pool.query('select * from задания;', (err, res)=>
+	{
+		if(err) console.log('Ошибка:', err)
+		else
+		{
+			console.log(res.rows)
+			pool.end()
+		}
+	}
+)
